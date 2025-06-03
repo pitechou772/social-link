fa99d406db846135f0bb943d2489160cb0285626
pitechou772 <144823740+pitechou772@users.noreply.github.com>
2025-06-03 11:35:06 +0200

    Add files via upload

---
+++ vanta-background.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+  VANTA.NET({
+    el: "#vanta-bg",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.0,
+    scaleMobile: 1.0,
+    color: 0xffa3b1,
+    backgroundColor: 0xfdfbfb,
+    points: 10.0,
+    maxDistance: 20.0,
+    spacing: 18.0
+  });
+});
+    
